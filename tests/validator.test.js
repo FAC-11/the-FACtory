c6d2c4b75eb36c0dd8537dbb0989443782cea28e
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-08-18 00:06:28 +0100

    Validator tests are now passing Relates #45

--- tests/validator.test.js
+++ tests/validator.test.js
@@ -3,7 +3,7 @@ const validator = require('../src/controllers/validator');
 
 test('checkFirstName', (t) => {
   // should not be empty
-  var expected = 'cant be empty';
+  var expected = "Please enter your firstname!";
   validator.checkFirstName('', (err, res) => {
     let actual = err.message;
     t.equals(actual, expected, 'Should give error if empty');
@@ -15,34 +15,34 @@ test('checkFirstName', (t) => {
     t.equals(actual, expected, 'Should not give error on valid input');
   });
   // should give error for numbers
-  var expected = "You're name should not contain non-alphabetical characters";
+  var expected = "You're name should not contain non-alphabetical characters!";
   validator.checkFirstName({ firstname: 'jack1' }, (err, res) => {
     var actual = err.message;
     t.equals(actual, expected, 'Should give error for numbers');
   });
     // should give error for spaces
-  var expected = "You're name should not contain non-alphabetical characters";
+  var expected = "You're name should not contain non-alphabetical characters!";
   validator.checkFirstName({ firstname: 'jack ' }, (err, res) => {
     var actual = err.message;
     t.equals(actual, expected, 'Should give error for spaces');
   });
   // should give error for special characters
-  var expected = "You're name should not contain non-alphabetical characters";
+  var expected = "You're name should not contain non-alphabetical characters!";
   validator.checkFirstName({ firstname: 'jack!@£$%^&*()' }, (err, res) => {
     var actual = err.message;
     t.equals(actual, expected, 'Should give error for special characters');
   });
   // should give error if more than 100 characters
-  var expected = "Character limit reached";
+  var expected = "You're name should not exceed 100 characters!";
   validator.checkFirstName({ firstname : 'qwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopqwertyuiopq' }, (err, res) => {
     let actual = err.message;
-    t.equals(actual, expected, 'Should give error if more than 100 characters');
+    t.equals(actual, expected, 'Should give error if more than 100 characters!');
     t.end();
   });
 });
 
 test('checkEmail', (t) => {
-  var expected = 'cant be empty';
+  var expected = 'Please enter your email!';
   validator.checkEmail('', (err, res) => {
     let actual = err.message;
     t.equals(actual, expected, 'Should give error if empty');
