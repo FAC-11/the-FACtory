5abf883a5394323d9612ec9a5fa7f7f8beb2651d
abdullahchaudhry <abdullah.chaudhry@outlook.com>
2017-08-17 17:40:08 +0100

    Refactored promise.js, removed validator functions Relates #41

--- src/controllers/promise.js
+++ src/controllers/promise.js
@@ -1,54 +1,13 @@
 const dbConnection = require('./../model/db_connection');
-
-const checkFirstName = data => {
-  const firstname = data.firstname;
-  const email = data.email;
-  const regex = /^[_A-z0-9]((-|\s)[_A-z0-9])*$/g;
-  if (!firstname) {
-    throw new Error('You need to write your name to continue')
-  } else if (firstname.match(regex)) {
-    throw new Error('Please do not use symbols')
-  } else if (firstname.length >= 100) {
-    throw new Error('Your username is too long!')
-  }
-}
-
-const checkEmail = data => {
-  const email = data.email;
-  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  if (!email) {
-    throw new Error('You need to write your email to continue')
-  } else if (!email.match(regex)) {
-    throw new Error('Your email address is not valid');
-  } else if (email.length >= 100) {
-    throw new Error('Your email is too long!')
-  }
-}
-
-const checkIdeaTitle = data => {
-  const title = data.ideatitle;
-  if (!title) {
-    throw new Error('Please write a name for your idea');
-  } else if (title.length >= 100) {
-    throw new Error('Your idea name is too long!')
-  }
-}
-
-const checkIdeaDescription = data => {
-  const desc = data.ideadesc;
-  if (!desc) {
-    throw new Error('Please describe your idea');
-  }
-}
+const validator = require('./validator');
 
 exports.postToDatabase = (postSQL, data) => {
-
   console.log('postToDB log');
   return new Promise((resolve, reject) => {
-    checkFirstName(data);
-    checkEmail(data);
-    checkIdeaTitle(data);
-    checkIdeaDescription(data);
+    validator.checkFirstName(data);
+    validator.checkEmail(data);
+    validator.checkIdeaTitle(data);
+    validator.checkIdeaDescription(data);
     dbConnection.query(postSQL,
       (err, res) => {
         if (err) {
---
+++ src/controllers/validator.js
@@ -0,0 +1,47 @@
+const checkFirstName = data => {
+  const firstname = data.firstname;
+  const email = data.email;
+  const regex = /^[_A-z0-9]((-|\s)[_A-z0-9])*$/g;
+  if (!firstname) {
+    throw new Error('You need to write your name to continue')
+  } else if (firstname.match(regex)) {
+    throw new Error('Please do not use symbols')
+  } else if (firstname.length >= 100) {
+    throw new Error('Your username is too long!')
+  }
+}
+
+const checkEmail = data => {
+  const email = data.email;
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (!email) {
+    throw new Error('You need to write your email to continue')
+  } else if (!email.match(regex)) {
+    throw new Error('Your email address is not valid');
+  } else if (email.length >= 100) {
+    throw new Error('Your email is too long!')
+  }
+}
+
+const checkIdeaTitle = data => {
+  const title = data.ideatitle;
+  if (!title) {
+    throw new Error('Please write a name for your idea');
+  } else if (title.length >= 100) {
+    throw new Error('Your idea name is too long!')
+  }
+}
+
+const checkIdeaDescription = data => {
+  const desc = data.ideadesc;
+  if (!desc) {
+    throw new Error('Please describe your idea');
+  }
+}
+
+module.exports = {
+  checkFirstName: checkFirstName,
+  checkEmail: checkEmail,
+  checkIdeaTitle,
+  checkIdeaDescription
+}
