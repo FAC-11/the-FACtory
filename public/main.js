a04deb2ed10ada1beabd06f12b40c21ceaf2876d
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-08-17 16:33:56 +0100

    stylying done #37

--- public/main.js
+++ public/main.js
@@ -1,13 +1,25 @@
-var icon = document.getElementById('js-icon');
-var isOn = false;
-icon.addEventListener('click', function(e) {
-  if (!isOn) {
-    icon.setAttribute('src', '/lightbulb-yellow.png');
-    document.body.style.backgroundColor = 'white';
-    isOn = true;
-  } else {
-    icon.src = '/lightbulb-white.png';
-    document.body.style.backgroundColor = '#2b2b2b';
-    isOn = false;
+(function () {
+  var icon = document.getElementById('js-icon');
+  var text = document.getElementsByClassName('text');
+  var isOn = false;
+  for(var i = 0; i < text.length; i++){
+    text[i].style.color = 'white';
   }
-});
+  icon.addEventListener('click', function(e) {
+    if (!isOn) {
+      icon.setAttribute('src', '/lightbulb-yellow.png');
+      document.body.style.backgroundColor = 'white';
+      for(var i = 0; i < text.length; i++){
+        text[i].style.color = 'black';
+      }
+      isOn = true;
+    } else {
+      icon.src = '/lightbulb-white.png';
+      document.body.style.backgroundColor = '#2b2b2b';
+      for(var i = 0; i < text.length; i++){
+        text[i].style.color = 'white';
+      }
+      isOn = false;
+    }
+  });
+})();
