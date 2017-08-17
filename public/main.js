f03e6756b6ce70fc7c5d0a91d2a21840b4a613f0
Max Gerber <mgerber94@gmail.com>
2017-08-17 15:58:11 +0100

    Added some button styling and lightbulb animations Relates #37

--- public/main.js
+++ public/main.js
@@ -1,1 +1,13 @@
-
+var icon = document.getElementById('js-icon');
+var isOn = false;
+icon.addEventListener('click', function(e) {
+  if (!isOn) {
+    icon.setAttribute('src', '/lightbulb-yellow.png');
+    document.body.style.backgroundColor = 'white';
+    isOn = true;
+  } else {
+    icon.src = '/lightbulb-white.png';
+    document.body.style.backgroundColor = '#2b2b2b';
+    isOn = false;
+  }
+});
