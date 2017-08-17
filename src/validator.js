31bb00ef6c14902dee29cde1a522776bb31feef2
Rebecacalvoquintero <rebecacalvoquintero@hotmail.es>
2017-08-17 12:16:03 +0100

    validator file added

--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,5 @@
 //validating username is inputted
-function validateUsername(username) {
-  var regex = /^[_A-z0-9]((-|\s)[_A-z0-9])*$/g;
+function validateFirstname(firstname) {
   if (!username.value) {
     //do stuff
   } else if (username.value.match(regex)) {
@@ -26,21 +25,36 @@ function validateEmail(email) {
   }
 }
 
-// validating password in put
-function validatePassword(password) {
-  var regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.[\W]).{8,}$/;
-  if (!password.value) {
-    valid = false;
-    var noPassword = document.createTextNode("Please enter your password")
-    return passwordError.appendChild(noPassword);
-  } else if (password.value.length < 12) {
-    valid = false;
-    var shortPassword = document.createTextNode("Your password is too short")
-    return passwordError.appendChild(shortPassword);
-  } else if (password.value.length >= 100) {
-    var longPassword = document.createTextNode("Your password is too long!");
-    return usernameError.appendChild(longPassword);
+  function validateIdeatitle(ideaTitle) {
+  valid = false;
+  if (!email.value) {
+    //do stuff
+  } else if (!email.value.includes('@tinfoild.com')) {
+    //do stuff
+  } else if (email.value.length >= 100) {
+    //do stuff
   } else {
-    valid = true;
+    //do stuff
   }
+}
+
+function validateIdeadesc(ideaDesc) {
+  valid = false;
+  if (!email.value) {
+    //do stuff
+  } else if (!email.value.includes('@tinfoild.com')) {
+    //do stuff
+  } else if (email.value.length >= 100) {
+    //do stuff
+  } else {
+    //do stuff
+  }
+}
+
+// validating password in put
+
+module.exports = {
+  validateFirstname,
+  validateEmail
+
 }
